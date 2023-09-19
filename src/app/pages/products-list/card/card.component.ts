dd6c501d13a26018344754eca989f3b43772b263
Alex <xskreol@yandex.by>
2023-09-19 22:07:40 +0300

    feat: add card

---
+++ src/app/pages/products-list/card/card.component.ts
@@ -0,0 +1,19 @@
+import {Component} from '@angular/core';
+import {IProduct} from '../../../shared/products/product.interface';
+import {productMock} from '../../../shared/products/product.mock';
+
+@Component({
+    selector: 'app-card',
+    templateUrl: './card.component.html',
+    styleUrls: ['./card.component.css'],
+})
+export class CardComponent {
+    productMock: IProduct = productMock;
+
+    buy(event: Event) {
+        event.stopPropagation();
+
+        // eslint-disable-next-line no-console
+        console.log('buy');
+    }
+}
