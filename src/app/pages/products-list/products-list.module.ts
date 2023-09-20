0f2161fa833c79f5885d228e8b8069e9026be283
Нанивская Екатерина Михайловна <nanivskaya_ek@mail.ru>
2023-09-20 22:05:16 +0300

    fix: убрала неиспользуемый модуль

--- src/app/pages/products-list/products-list.module.ts
+++ src/app/pages/products-list/products-list.module.ts
@@ -1,12 +1,11 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {MatCardModule} from '@angular/material/card';
 import {ProductsListComponent} from './products-list.component';
 import {CardComponent} from './card/card.component';
 
 @NgModule({
     declarations: [ProductsListComponent, CardComponent],
-    imports: [CommonModule, MatCardModule],
+    imports: [CommonModule],
     exports: [ProductsListComponent],
 })
 export class ProductsListModule {}
