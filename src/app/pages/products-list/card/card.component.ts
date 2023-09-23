4360c87741fa8a3679a2022e04434968346538e5
AXBIT\ifokin <ifokin@axbit.ru>
2023-09-23 09:40:57 +0300

    feat: add card component

---
+++ src/app/pages/products-list/card/card.component.ts
@@ -0,0 +1,18 @@
+import {Component} from '@angular/core';
+import {IProduct} from '../../../shared/products/product.interface';
+import {productMock} from '../../../shared/products/product.mock';
+
+@Component({
+    selector: 'app-card',
+    templateUrl: './card.component.html',
+    styleUrls: ['./card.component.css'],
+})
+export class CardComponent {
+    protected product: IProduct = productMock;
+
+    protected byeProduct(event: Event): void {
+        event.stopPropagation();
+        // eslint-disable-next-line no-console
+        console.log('Bye product');
+    }
+}
