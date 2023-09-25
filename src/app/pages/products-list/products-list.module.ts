dfa72b9e6552cbb3c637725477f91d61d19f5b93
Нанивская Екатерина Михайловна <nanivskaya_ek@mail.ru>
2023-09-25 11:54:48 +0300

    fix: правки по MR (добавила модуль)

--- src/app/pages/products-list/products-list.module.ts
+++ src/app/pages/products-list/products-list.module.ts
@@ -1,11 +1,11 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {ProductsListComponent} from './products-list.component';
-import {CardComponent} from './card/card.component';
+import {CardModule} from './card/card.module';
 
 @NgModule({
-    declarations: [ProductsListComponent, CardComponent],
-    imports: [CommonModule],
+    declarations: [ProductsListComponent],
+    imports: [CommonModule, CardModule],
     exports: [ProductsListComponent],
 })
 export class ProductsListModule {}
