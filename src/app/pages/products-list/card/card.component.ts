58fa3fff5d1bb665583c6eb2e6c0d64ac79bf3e2
Olga Garder <o.garder@blanc.ru>
2023-09-28 15:14:32 +0300

    ci: initialised basic js project, add Input and Output decorator

--- src/app/pages/products-list/card/card.component.ts
+++ src/app/pages/products-list/card/card.component.ts
@@ -1,5 +1,5 @@
-import {Component} from '@angular/core';
-import {productsMock} from '../../../shared/products/products.mock';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {IProduct} from '../../../shared/products/product.interface';
 
 @Component({
     selector: 'app-card',
@@ -7,16 +7,15 @@ import {productsMock} from '../../../shared/products/products.mock';
     styleUrls: ['./card.component.css'],
 })
 export class CardComponent {
-    readonly product = productsMock[0];
+    @Input() product: IProduct | null = null;
+    @Output() buyBtnClick: EventEmitter<any> = new EventEmitter<any>();
 
     onProductBuy(event: Event) {
         event.stopPropagation();
-
-        // eslint-disable-next-line no-console
-        console.log('Buy product');
+        this.buyBtnClick.emit(this.product);
     }
 
     isStarActive(starIndex: number): boolean {
-        return this.product.rating >= starIndex;
+        return this.product ? this.product.rating >= starIndex : false;
     }
 }
--- src/app/pages/products-list/products-list.component.ts
+++ src/app/pages/products-list/products-list.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {productsMock} from '../../shared/products/products.mock';
 
 @Component({
     selector: 'app-products-list',
@@ -7,8 +8,10 @@ import {Component} from '@angular/core';
     // encapsulation: ViewEncapsulation.Emulated,
 })
 export class ProductsListComponent {
-    onCardClick() {
+    readonly products = productsMock;
+
+    onBuyBtnClick(productId: string) {
         // eslint-disable-next-line no-console
-        console.log('Card click');
+        console.log(this.products.find(x => x._id === productId));
     }
 }
