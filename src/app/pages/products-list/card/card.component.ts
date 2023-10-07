3493a55f46463fc9fc7fda912cb557a0f740ee6f
vladislav.golodok <vladislav.golodok@ax-team.com>
2023-10-07 21:55:49 +0300

    feat: resolve comments

--- src/app/pages/products-list/card/card.component.ts
+++ src/app/pages/products-list/card/card.component.ts
@@ -1,17 +1,20 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
-import {IProduct} from '../../../shared/products/product.interface';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+import { IProduct } from '../../../shared/products/product.interface';
 
 @Component({
-    selector: 'app-card',
-    templateUrl: './card.component.html',
-    styleUrls: ['./card.component.css'],
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css']
 })
 export class CardComponent {
-    @Input() product?: IProduct;
-    @Output() productToBucket: EventEmitter<IProduct> = new EventEmitter();
+  @Input() product: IProduct | null = null;
+  @Output() readonly productToBucket: EventEmitter<IProduct> = new EventEmitter();
 
-    addToBasket(event: Event): void {
-        event.stopPropagation();
-        this.productToBucket.emit(this.product);
+  addToBasket(event: Event): void {
+    event.stopPropagation();
+    if (this.product !== null) {
+      this.productToBucket.emit(this.product);
     }
+  }
 }
--- src/app/pages/products-list/products-list.component.ts
+++ src/app/pages/products-list/products-list.component.ts
@@ -1,23 +1,24 @@
-import {Component} from '@angular/core';
-import {IProduct} from '../../shared/products/product.interface';
-import {productMock} from '../../shared/products/product.mock';
+import { Component } from '@angular/core';
+
+import { IProduct } from '../../shared/products/product.interface';
+import { productMock } from '../../shared/products/product.mock';
 
 @Component({
-    selector: 'app-products-list',
-    templateUrl: './products-list.component.html',
-    styleUrls: ['./products-list.component.css'],
+  selector: 'app-products-list',
+  templateUrl: './products-list.component.html',
+  styleUrls: ['./products-list.component.css']
 })
 export class ProductsListComponent {
-    products: IProduct[] = productMock;
-    private bucket?: IProduct[] = [];
+  products: IProduct[] = productMock;
+  private bucket: IProduct[] = [];
 
-    getBucketItem(item: IProduct): void {
-        const findItem = this.bucket?.find(value => value === item);
+  getBucketItem(item: IProduct): void {
+    const findItem = this.bucket.find(value => value === item);
 
-        if (!findItem) {
-            this.bucket?.push(item);
-        } else {
-            this.bucket = this.bucket?.filter(value => value !== item);
-        }
+    if (!findItem) {
+      this.bucket.push(item);
+    } else {
+      this.bucket = this.bucket.filter(value => value !== item);
     }
+  }
 }
