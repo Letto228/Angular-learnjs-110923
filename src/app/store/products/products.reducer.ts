55864b85ca53700d6ec50e46becd087ff251dcd0
Letto228 <learnjs@MacBook-Pro-Egor.local>
2023-10-26 15:36:58 +0300

    chore: sanitize project

--- src/app/store/products/products.reducer.ts
+++ src/app/store/products/products.reducer.ts
@@ -4,14 +4,6 @@ import {addProducts} from './products.actions';
 
 export const productsReducer = createReducer(
     productsInitialState,
-    // on(addProducts, (state: IProductsState, action) => {
-    //     const newState: IProductsState = {
-    //         ...state,
-    //         data: action.products,
-    //     };
-
-    //     return newState;
-    // }),
     on(addProducts, (state, {products}) => ({
         ...state,
         data: products,
